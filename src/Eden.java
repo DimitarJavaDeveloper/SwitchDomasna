7fcc2b28a6b9a25017049790e263cd77bcfaab1d
unknown <dimitarbelcovski29@gmail.com>
2023-04-10 11:18:47 +0200

    Add files

---
+++ src/Eden.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class najgolembroj {
+ public static void main(String[] args) {
+ Scanner input = new Scanner(System.in);
+ System.out.println("Najgolem od n vcitani broevi");
+ int n, x, max;
+ System.out.print("Kolku broevi ke proveruvas ");
+ n = input.nextInt();
+ System.out.print("Vnesi 1. broj ");
+ x = input.nextInt();
+ max = x;
+ for (int i = 2; i <= n; i++) {
+ System.out.print("Vnesi " + i + ". broj ");
+ x = input.nextInt();
+ if (x > max)
+ max = x;
+ }
+ System.out.println("Najgolem broj e " + max);
+ }
+}
